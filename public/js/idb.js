a6429908e8e79279a3ffcd3769eeae618f4618ad
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-11-15 15:15:56 -0600

    functions working on server.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,63 +1,45 @@
-//Here, we create a variable db that will store the connected database object when the connection is complete. After that, we create the request variable to act as an event listener for the database. That event listener is created when we open the connection to the database using the indexedDB.open() method.
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
-// create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'offline_budget' and set it to version 1
-const request = indexedDB.open("offline_budget", 1);
+const request = indexedDB.open("budget", 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function (event) {
-  // save a reference to the database
-  const db = event.target.result;
-  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
-  db.createObjectStore("new_transaction", { autoIncrement: true });
+request.onupgradeneeded = (event) => {
+  event.target.result.createObjectStore("pending", {
+    keyPath: "id",
+    autoIncrement: true,
+  });
 };
 
-// upon a successful
-request.onsuccess = function (event) {
-  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-  db = event.target.result;
+request.onerror = (err) => {
+  console.log(err.message);
+};
 
-  // check if app is online, if yes run uploadTransaction() function to send all local db data to api
+request.onsuccess = (event) => {
+  db = event.target.result;
   if (navigator.onLine) {
-    uploadTransaction();
+    checkDatabase();
   }
 };
 
-request.onerror = function (event) {
-  // log error here
-  console.log(event.target.errorCode);
-};
-
-// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
-  // open a new transaction with the database with read and write permissions
-  const transaction = db.transaction(["new_transaction"], "readwrite");
-
-  // access the object store for `new_transaction`
-  const transactionObjectStore = transaction.objectStore("new_transaction");
-
-  // add record to your store with add method
-  transactionObjectStore.add(record);
-
-  //where in index.js do I need to call this function?
+  const transaction = db.transaction("pending", "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
 
-function uploadTransaction() {
-  // open a transaction on your db
-  const transaction = db.transaction(["new_transaction"], "readwrite");
+function checkDatabase() {
+  const transaction = db.transaction("pending", "readonly");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
 
-  // access your object store
-  const transactionObjectStore = transaction.objectStore("new_transaction");
-
-  // get all records from store and set to a variable
-  const getAll = transactionObjectStore.getAll();
-
-  // upon a successful .getAll() execution, run this function
-  getAll.onsuccess = function () {
-    // if there was data in indexedDb's store, let's send it to the api server
+  getAll.onsuccess = () => {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
@@ -66,26 +48,13 @@ function uploadTransaction() {
         },
       })
         .then((response) => response.json())
-        .then((serverResponse) => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-          // open one more transaction
-          const transaction = db.transaction(["new_transaction"], "readwrite");
-          // access the new_transaction object store
-          const transactionObjectStore =
-            transaction.objectStore("new_transaction");
-          // clear all items in your store
-          transactionObjectStore.clear();
-
-          alert("All saved transactions have been submitted!");
-        })
-        .catch((err) => {
-          console.log(err);
+        .then(() => {
+          const transaction = db.transaction("pending", "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
         });
     }
   };
 }
 
-// listen for app coming back online
-window.addEventListener("online", uploadTransaction);
+window.addEventListener("online", checkDatabase);
